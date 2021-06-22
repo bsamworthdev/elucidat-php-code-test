7b27ac2f5a2b62240dab8e529868a41ebef35e99
Ben Samworth <bsamworthdev@outlook.co.uk>
2021-06-22 22:48:15 +0100

    Simplified business logic

--- src/GildedRose.php
+++ src/GildedRose.php
@@ -39,10 +39,7 @@ class GildedRose
                 case $backStage:
                     if ($item->quality < 50) {
                         $item->quality += 1;
-                        if ($item->sellIn < 11 && $item->quality < 50) {
-                            $item->quality += 1;
-                        }
-                        if ($item->sellIn < 6 && $item->quality < 50) {
+                        if ($item->sellIn < 11) {
                             $item->quality += 1;
                         }
                     }
@@ -57,11 +54,9 @@ class GildedRose
                     if ($item->quality > 0) {
                         $item->quality -= 1;
                     }
-                    $item->sellIn = $item->sellIn - 1;
-                    if ($item->sellIn < 0) {
-                        if ($item->quality > 0) {
-                            $item->quality = $item->quality - 1;
-                        }
+                    $item->sellIn -= 1;
+                    if ($item->sellIn < 0 && $item->quality > 0) {
+                            $item->quality -= 1;
                     }
                     break;
             }
