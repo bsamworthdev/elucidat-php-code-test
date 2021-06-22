036f4620c3ed1d3202b2ee907eefcc8b7fdabe94
Ben Samworth <bsamworthdev@outlook.co.uk>
2021-06-22 22:40:21 +0100

    Refactored to separate out logic for different items

--- src/GildedRose.php
+++ src/GildedRose.php
@@ -21,54 +21,95 @@ class GildedRose
 
     public function nextDay()
     {
-        const $backStage = 'Backstage passes to a TAFKAL80ETC concert';
-        const $sulfuras = 'Sulfuras, Hand of Ragnaros';
-        const $brie = 'Aged Brie';
+        $backStage = 'Backstage passes to a TAFKAL80ETC concert';
+        $sulfuras = 'Sulfuras, Hand of Ragnaros';
+        $brie = 'Aged Brie';
         
         foreach ($this->items as $item) {
-            if ($item->name != $brie and $item->name != $backStage) {
+            if ($item->name == $brie){
+                if ($item->quality < 50) {
+                    $item->quality += 1;
+                }
+                $item->sellIn -= 1;
+                if ($item->sellIn < 0 && $item->quality < 50) {
+                    $item->quality += 1;
+                }
+            } elseif ($item->name == $backStage){
+                if ($item->quality < 50) {
+                    $item->quality += 1;
+                    if ($item->sellIn < 11 && $item->quality < 50) {
+                        $item->quality += 1;
+                    }
+                    if ($item->sellIn < 6 && $item->quality < 50) {
+                        $item->quality += 1;
+                    }
+                }
+                $item->sellIn = $item->sellIn - 1;
+                if ($item->sellIn < 0) {
+                    $item->quality = $item->quality - $item->quality;
+                }
+            } elseif ($item->name == $sulfuras){
+                
+            } else {
                 if ($item->quality > 0) {
-                    if ($item->name != $sulfuras) {
+                    $item->quality -= 1;
+                }
+                $item->sellIn = $item->sellIn - 1;
+                if ($item->sellIn < 0) {
+                    if ($item->quality > 0) {
                         $item->quality = $item->quality - 1;
                     }
                 }
-            } else {
-                if ($item->quality < 50) {
-                    $item->quality = $item->quality + 1;
-                    if ($item->name == $backStage) {
-                        if ($item->sellIn < 11) {
-                            if ($item->quality < 50) {
-                                $item->quality = $item->quality + 1;
-                            }
-                        }
-                        if ($item->sellIn < 6) {
-                            if ($item->quality < 50) {
-                                $item->quality = $item->quality + 1;
-                            }
-                        }
-                    }
-                }
             }
-            if ($item->name != $sulfuras) {
-                $item->sellIn = $item->sellIn - 1;
-            }
-            if ($item->sellIn < 0) {
-                if ($item->name != $brie) {
-                    if ($item->name != $backStage) {
-                        if ($item->quality > 0) {
-                            if ($item->name != $sulfuras) {
-                                $item->quality = $item->quality - 1;
-                            }
-                        }
-                    } else {
-                        $item->quality = $item->quality - $item->quality;
-                    }
-                } else {
-                    if ($item->quality < 50) {
-                        $item->quality = $item->quality + 1;
-                    }
-                }
-            }
+    
+    
+    
+
+
+
+            // if ($item->name != $brie and $item->name != $backStage) {
+            //     // if ($item->quality > 0) {
+            //     //     if ($item->name != $sulfuras) {
+            //     //         $item->quality = $item->quality - 1;
+            //     //     }
+            //     // }
+            // } else {
+            //     // if ($item->quality < 50) {
+            //     //     $item->quality = $item->quality + 1;
+            //     //     if ($item->name == $backStage) {
+            //     //         if ($item->sellIn < 11) {
+            //     //             if ($item->quality < 50) {
+            //     //                 $item->quality = $item->quality + 1;
+            //     //             }
+            //     //         }
+            //     //         if ($item->sellIn < 6) {
+            //     //             if ($item->quality < 50) {
+            //     //                 $item->quality = $item->quality + 1;
+            //     //             }
+            //     //         }
+            //     //     }
+            //     // }
+            // }
+            // if ($item->name != $sulfuras) {
+            //     // $item->sellIn = $item->sellIn - 1;
+            // }
+            // if ($item->sellIn < 0) {
+            //     // if ($item->name != $brie) {
+            //     //     if ($item->name != $backStage) {
+            //     //         if ($item->quality > 0) {
+            //     //             if ($item->name != $sulfuras) {
+            //     //                 $item->quality = $item->quality - 1;
+            //     //             }
+            //             }
+            //         } else {
+            //             // $item->quality = $item->quality - $item->quality;
+            //         }
+            //     } else {
+            //         // if ($item->quality < 50) {
+            //         //     $item->quality = $item->quality + 1;
+            //         // }
+            //     }
+            // }
         }
     }
 }
